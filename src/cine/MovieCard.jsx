dd874ff672f517e6bb9b4719301a4c97a83f4ea6
zunayed <zunayed.arp@gmail.com>
2025-09-17 08:39:57 +0200

    chore:new movie card components added

--- src/cine/MovieCard.jsx
+++ src/cine/MovieCard.jsx
@@ -1,3 +1,32 @@
-export default function MovieCard() {
-  return <h1>Movie Card</h1>;
+import Star from "../assets/star.svg";
+import { getImgUrl } from "../utils/cine-utility";
+
+export default function MovieCard({ movie }) {
+  return (
+    <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
+      <img
+        className="w-full object-cover"
+        src={getImgUrl(movie.cover)}
+        alt={movie.title}
+      />
+      <figcaption className="pt-4">
+        <h3 className="text-xl mb-1">{movie.title}</h3>
+        <p className="text-[#575A6E] text-sm mb-2">{movie.genre}</p>
+        <div className="flex items-center space-x-1 mb-5">
+          <img src={Star} width="14" height="14" alt="" />
+          <img src={Star} width="14" height="14" alt="" />
+          <img src={Star} width="14" height="14" alt="" />
+          <img src={Star} width="14" height="14" alt="" />
+          <img src={Star} width="14" height="14" alt="" />
+        </div>
+        <a
+          className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
+          href="#"
+        >
+          <img src={null} alt="" />
+          <span>{movie.price} | Add to Cart</span>
+        </a>
+      </figcaption>
+    </figure>
+  );
 }
--- src/cine/MovieList.jsx
+++ src/cine/MovieList.jsx
@@ -1,6 +1,5 @@
-import Star from "../assets/star.svg";
 import { getAllMovies } from "../data/movies";
-import { getImgUrl } from "../utils/cine-utility";
+import MovieCard from "./MovieCard";
 
 export default function MovieList() {
   const movies = getAllMovies();
@@ -9,34 +8,7 @@ export default function MovieList() {
     <div className="content">
       <div className="grid sm:grid-cols-2 xl:grid-cols-3 gap-7">
         {movies.map((movie) => (
-          <figure
-            key={movie.id}
-            className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl"
-          >
-            <img
-              className="w-full object-cover"
-              src={getImgUrl(movie.cover)}
-              alt=""
-            />
-            <figcaption className="pt-4">
-              <h3 className="text-xl mb-1">{movie.title}</h3>
-              <p className="text-[#575A6E] text-sm mb-2">{movie.genre}</p>
-              <div className="flex items-center space-x-1 mb-5">
-                <img src={Star} width="14" height="14" alt="" />
-                <img src={Star} width="14" height="14" alt="" />
-                <img src={Star} width="14" height="14" alt="" />
-                <img src={Star} width="14" height="14" alt="" />
-                <img src={Star} width="14" height="14" alt="" />
-              </div>
-              <a
-                className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
-                href="#"
-              >
-                <img src="" alt="" />
-                <span>{movie.price} | Add to Cart</span>
-              </a>
-            </figcaption>
-          </figure>
+          <MovieCard key={movie.id} movie={movie} />
         ))}
       </div>
     </div>
