1b9f52f463d97e6d79e1c79719b06fc9a2d2936f
zunayed <zunayed.arp@gmail.com>
2025-09-17 10:14:23 +0200

    chore:cart details and modal added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import Moon from "../assets/icons/moon.svg";
 import Logo from "../assets/logo.svg";
 import Ring from "../assets/ring.svg";
 import ShoppingCart from "../assets/shopping-cart.svg";
+import CartDetails from "../cine/CartDetails";
 
 export default function Header() {
+  const [showCart, setShowCart] = useState(false);
+  const handleCartShow = () => {
+    setShowCart(true);
+  };
+
   return (
     <header>
+      {showCart && <CartDetails onClose={() => setShowCart(false)} />}
       <nav className="container flex items-center justify-between space-x-10 py-6">
         <a href="index.html">
           <img src={Logo} width="139" height="26" alt="logo" />
@@ -32,6 +40,7 @@ export default function Header() {
             <a
               className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
               href="#"
+              onClick={handleCartShow}
             >
               <img src={ShoppingCart} width="24" height="24" alt="cart" />
             </a>
