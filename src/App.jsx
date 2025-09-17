e4148aedf48a69355cdbd23b23f74a2716ce8171
zunayed <zunayed.arp@gmail.com>
2025-09-17 10:29:13 +0200

    chore:created and implementing context

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
+import { useState } from "react";
 import MovieList from "./cine/MovieList";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
+import { MovieContext } from "./context";
 
 function App() {
+  const { cartData, setCartData } = useState([]);
+
   return (
     <>
-      <Header />
-      <main>
-        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
-          <Sidebar />
-          <MovieList />
-        </div>
-      </main>
-      <Footer />
+      <MovieContext.Provider value={{ cartData, setCartData }}>
+        <Header />
+        <main>
+          <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+            <Sidebar />
+            <MovieList />
+          </div>
+        </main>
+        <Footer />
+      </MovieContext.Provider>
     </>
   );
 }
