7da4056864b26cafb58ef903d713292cff4c0827
zunayed <zunayed.arp@gmail.com>
2025-09-17 08:49:18 +0200

    chore:create and added rating components

--- src/cine/MovieCard.jsx
+++ src/cine/MovieCard.jsx
@@ -1,5 +1,5 @@
-import Star from "../assets/star.svg";
 import { getImgUrl } from "../utils/cine-utility";
+import Rating from "./Rating";
 
 export default function MovieCard({ movie }) {
   return (
@@ -13,11 +13,7 @@ export default function MovieCard({ movie }) {
         <h3 className="text-xl mb-1">{movie.title}</h3>
         <p className="text-[#575A6E] text-sm mb-2">{movie.genre}</p>
         <div className="flex items-center space-x-1 mb-5">
-          <img src={Star} width="14" height="14" alt="" />
-          <img src={Star} width="14" height="14" alt="" />
-          <img src={Star} width="14" height="14" alt="" />
-          <img src={Star} width="14" height="14" alt="" />
-          <img src={Star} width="14" height="14" alt="" />
+          <Rating value={movie.rating} />
         </div>
         <a
           className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
